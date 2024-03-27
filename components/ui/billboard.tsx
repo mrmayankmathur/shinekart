438aeab7dc9f26965796a7c865b79b5ba3415258
Mayank Mathur <139607998+mrmayankmathur@users.noreply.github.com>
2024-03-27 20:10:51 +0530

    billboard.tsx

--- components/ui/billboard.tsx
+++ components/ui/billboard.tsx
@@ -4,26 +4,21 @@ interface BillboardProps {
   data: Billboard;
 }
 
-const Billboard: React.FC<BillboardProps> = ({ data }) => {
-  // Check if data is defined and has the label property
-  if (!data || !data.label) {
-    return null; // or render a placeholder, error message, or loading indicator
-  }
-
-  return (
+const Billboard: React.FC<BillboardProps> = ({
+  data
+}) => {
+  console
+  return ( 
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div
-        style={{ backgroundImage: `url(${data.imageUrl})` }}
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover"
-      >
+      <div style={{ backgroundImage: `url(${data?.imageUrl})` }} className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover">
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs" style={{ WebkitTextStroke: '1px rgba(211, 211, 211, 0.25)' }}>
+          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
             {data.label}
           </div>
         </div>
       </div>
     </div>
-  );
+   );
 };
 
 export default Billboard;
